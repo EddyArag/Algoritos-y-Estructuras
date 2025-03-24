c43fcce74469caf9e729f845cb47eb8d42638126
EddyArag <eddypaularagon@gmail.com>
2025-03-24 18:00:44 -0500

    EjercicioCLase

--- SESION2/Codigos/Ejercicios/src/EjercicioEnClase/BolsasNumeros.java
+++ SESION2/Codigos/Ejercicios/src/EjercicioEnClase/BolsasNumeros.java
@@ -1,5 +1,30 @@
 package EjercicioEnClase;
 
-public class BolsasNumeros {
+import java.util.ArrayList;
+import java.util.List;
+
+class BolsasNumeros<N extends Number & Comparable<N>> {
+    private List<N> numeros;
+
+    public BolsasNumeros() {
+        this.numeros = new ArrayList<>();
+    }
+
+    public void agregarNumero(N numero) {
+        int pos = 0;
+        while (pos < numeros.size() && numeros.get(pos).compareTo(numero)<0{
+            pos++;
+        }
+        numeros.add(pos,numero);
+    }
+
+    public List<N> obtenerNumeros() {
+        return numeros;
+    }
+
+    @Override
+    public String toString() {
+        return "BolsaNumero{" + "numeros = " + numeros + '}';
+    }
 
 }
---
+++ SESION2/Codigos/Ejercicios/src/EjercicioEnClase/Main.java
@@ -0,0 +1,14 @@
+package EjercicioEnClase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BolsasNumeros<Integer> bolsita1 = new BolsasNumeros<>();
+        bolsita1.agregarNumero(4);
+        bolsita1.agregarNumero(3);
+        bolsita1.agregarNumero(9);
+    }
+}
