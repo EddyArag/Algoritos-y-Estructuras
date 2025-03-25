a135e209b4aeed30dff82eb56c2aab57ef90579a
EddyArag <164369960+EddyArag@users.noreply.github.com>
2025-03-24 23:07:01 -0500

    Update Cajoneria.java

--- SESION2/Codigos/Ejercicios/src/EjerciciosFinal/Cajoneria.java
+++ SESION2/Codigos/Ejercicios/src/EjerciciosFinal/Cajoneria.java
@@ -1,5 +1,27 @@
 package EjerciciosFinal;
 
-public class Cajoneria {
+// Ejercicio 3: Implementación de la clase Caja y la clase Cajoneria
+import java.util.ArrayList;
+import java.util.Iterator;
 
+// Clase Cajoneria que almacena múltiples cajas con diferentes objetos
+class Cajoneria implements Iterable<Caja<?>> {
+    private ArrayList<Caja<?>> lista = new ArrayList<>();
+    private int tope;
+
+    public Cajoneria(int tope) {
+        this.tope = tope;
+    }
+
+    public void add(Caja<?> caja) {
+        if (lista.size() < tope) {
+            lista.add(caja);
+        } else {
+            throw new RuntimeException("No caben más cajas en la cajoneria");
+        }
+    }
+
+    public Iterator<Caja<?>> iterator() {
+        return lista.iterator();
+    }
 }
