82c6b4060a2e5ba3ca422b32790fa39c310e97fa
EddyArag <164369960+EddyArag@users.noreply.github.com>
2025-04-28 00:13:13 -0500

    WASA

--- SESION05/Ejercicio/Lista.java
+++ SESION05/Ejercicio/Lista.java
@@ -1,28 +1,37 @@
 package SESION05.Ejercicio;
 
-public class Lista<T extends Comparable<T>>{
+public class Lista<T extends Comparable<T>> {
     private Node<T> first;
-    //Constructor de la clase Lista
-    public Lista(){
+
+    // Constructor de la clase Lista
+    public Lista() {
         this.first = null;
     }
-    //Método para verificar si la lista está vacía
+
+    // Método para verificar si la lista está vacía
     public boolean isEmptyList() {
-        if(first == null){
+        if (first == null) {
             return true;
         }
         return false;
     }
-    //Setters y getters de first
-    public void setFirst(Node<T> first) { this.first = first; }
-    public Node<T> getFirst() { return first; }
-    //Método para mostrar los elementos de la lista
-    public void mostrarElementos(){
-        if(first == null) {
+
+    // Setters y getters de first
+    public void setFirst(Node<T> first) {
+        this.first = first;
+    }
+
+    public Node<T> getFirst() {
+        return first;
+    }
+
+    // Método para mostrar los elementos de la lista
+    public void mostrarElementos() {
+        if (first == null) {
             System.out.println("Lista vacía.");
         } else {
             Node<T> actual = first;
-            while(actual.getNext() != null){
+            while (actual != null) {
                 System.out.print(actual.getElemento() + " ");
                 actual = actual.getNext();
             }
--- SESION05/Ejercicio/ListasEnlazadas.java
+++ SESION05/Ejercicio/ListasEnlazadas.java
@@ -1,16 +1,17 @@
 package SESION05.Ejercicio;
+
 import java.util.Random;
 
 public class ListasEnlazadas<T extends Comparable<T>> {
-    //Método para buscar un elemento genérico en la lista
-    public int buscarElemento(Lista<T> lista, T elemento){
-        if(lista.isEmptyList()){
+    // Método para buscar un elemento genérico en la lista
+    public int buscarElemento(Lista<T> lista, T elemento) {
+        if (lista.isEmptyList()) {
             return -1;
-        } else{
+        } else {
             int cont = 0;
             Node<T> actual = lista.getFirst();
-            while(actual.getNext() != null){
-                if(actual.getElemento().equals(elemento)){
+            while (actual != null) {
+                if (actual.getElemento().equals(elemento)) {
                     return cont;
                 }
                 cont++;
@@ -18,54 +19,66 @@ public class ListasEnlazadas<T extends Comparable<T>> {
             return -1;
         }
     }
-    //Método para invertir una lista genérica
+
+    // Método para invertir una lista genérica
     public Lista<T> invertirLista(Lista<T> lista) {
-        if(lista.isEmptyList()){
+        if (lista.isEmptyList()) {
             return null;
         } else {
             Lista<T> nuevaLista = new Lista<>();
             Node<T> actual = lista.getFirst();
-            while(actual.getNext() != null){
-                insertarAlFinal(nuevaLista.getFirst(), actual.getElemento());
+            // nuevaLista.setFirst(actual);
+            while (actual != null) {
+                insertarAlInicio(nuevaLista, actual.getElemento());
                 actual = actual.getNext();
             }
             return nuevaLista;
         }
     }
-    //Método para insertar un nodo al final
-    public void insertarAlFinal(Node<T> first, T elemento){
+
+    // Método para insertar un nodo al inicio
+    public void insertarAlInicio(Lista<T> lista, T elemento) {
+        Node<T> nuevo = new Node<>(elemento);
+        nuevo.setNext(lista.getFirst());
+        lista.setFirst(nuevo);
+    }
+
+    // Método para insertar un nodo al final
+    public void insertarAlFinal(Node<T> first, T elemento) {
         Node<T> actual = first;
-        while(actual.getNext() != null){
+        while (actual.getNext() != null) {
             actual = actual.getNext();
         }
         actual.setNext(new Node<>(elemento));
     }
-    //Método para contar el total de nodos en una lista enlazada
-    public int contarNodos(Lista<T> lista){
-        if(lista.isEmptyList()){
+
+    // Método para contar el total de nodos en una lista enlazada
+    public int contarNodos(Lista<T> lista) {
+        if (lista.isEmptyList()) {
             return 0;
         } else {
             int cont = 0;
             Node<T> actual = lista.getFirst();
-            while(actual.getNext() != null){
+            while (actual != null) {
                 actual = actual.getNext();
                 cont++;
             }
             return cont;
         }
     }
-    //Método para comparar dos listas enlazadas genéricas
-    public void sonIguales(Lista<T> lista1, Lista<T> lista2){
-        if(lista1.isEmptyList() || lista2.isEmptyList()){
+
+    // Método para comparar dos listas enlazadas genéricas
+    public void sonIguales(Lista<T> lista1, Lista<T> lista2) {
+        if (lista1.isEmptyList() || lista2.isEmptyList()) {
             System.out.println("Alguna lista está vacía.");
         } else {
-            if(contarNodos(lista1) == contarNodos(lista2)) {
+            if (contarNodos(lista1) == contarNodos(lista2)) {
                 Node<T> actual1 = lista1.getFirst();
                 Node<T> actual2 = lista2.getFirst();
-                while(actual1.getNext() != null) {
-                    if(actual1.getElemento().compareTo(actual2.getElemento()) != 0){
+                while (actual1 != null) {
+                    if (actual1.getElemento().compareTo(actual2.getElemento()) != 0) {
                         System.out.println("Las listas no son iguales.");
-                        break;
+                        return;
                     }
                     actual1 = actual1.getNext();
                     actual2 = actual2.getNext();
@@ -73,26 +86,28 @@ public class ListasEnlazadas<T extends Comparable<T>> {
                 System.out.println("Las lista son iguales.");
             } else {
                 System.out.println("Las listas no tienen la misma cantidad de elementos.");
-            } 
+            }
         }
     }
-    //Método para concatenar dos listas enlazadas
+
+    // Método para concatenar dos listas enlazadas
     public Lista<T> concatenarListas(Lista<T> lista1, Lista<T> lista2) {
-        if(lista1.isEmptyList() || lista2.isEmptyList()){
+        if (lista1.isEmptyList() || lista2.isEmptyList()) {
             return new Lista<>();
         } else {
             Node<T> actual1 = lista1.getFirst();
-            while(actual1.getNext() != null){
+            while (actual1.getNext() != null) {
                 actual1 = actual1.getNext();
             }
             actual1.setNext(lista2.getFirst());
             return lista1;
         }
     }
+
     public static void main(String[] args) {
         ListasEnlazadas<Integer> listasInt = new ListasEnlazadas<>();
         Random random1 = new Random();
-        
+
         Lista<Integer> listaInt1 = new Lista<>();
         Lista<Integer> listaInt2 = new Lista<>();
 
@@ -125,7 +140,7 @@ public class ListasEnlazadas<T extends Comparable<T>> {
         int nodosListaInt1 = listasInt.contarNodos(listaInt1);
         System.out.println("Nodos en lista1: " + nodosListaInt1);
 
-        //Uso del método mostrarElementos para mostrar ambas listas antes de comparar
+        // Uso del método mostrarElementos para mostrar ambas listas antes de comparar
         System.out.print("Lista 1 después de comparar: ");
         listaInt1.mostrarElementos();
         System.out.println();
@@ -139,7 +154,6 @@ public class ListasEnlazadas<T extends Comparable<T>> {
         // 4. insertAlFinal en lista1 (agregar 1) y lista2 (agregar 2)
         listasInt.insertarAlFinal(listaInt1.getFirst(), 24);
         listasInt.insertarAlFinal(listaInt2.getFirst(), 35);
-        listasInt.insertarAlFinal(listaInt2.getFirst(), 10);
 
         // Mostrar elementos después de segunda comparación
         System.out.print("Lista 1 después de segunda comparación: ");
@@ -159,21 +173,28 @@ public class ListasEnlazadas<T extends Comparable<T>> {
         // 6. contarNodos en lista2
         int nodosListaInt2 = listasInt.contarNodos(listaInt2);
         System.out.println("Nodos en lista2 después de insertar: " + nodosListaInt2);
-
+        // INVERSION DE LISTA 1
+        System.out.println("ACTUAL LISTA 1");
+        listaInt1.mostrarElementos();
+        System.out.println();
+        System.out.println("Inversion de lista 1");
+        Lista<Integer> listaInversaInt = listasInt.invertirLista(listaInt1);
+        listaInversaInt.mostrarElementos();
+        System.out.println();
         // 7. concatenarListas
         Lista<Integer> listaConcatenadaInt = listasInt.concatenarListas(listaInt1, listaInt2);
-        System.out.print("Lista concatenada: ");
+        System.out.println("Lista concatenada: ");
         listaConcatenadaInt.mostrarElementos();
         System.out.println();
 
         ListasEnlazadas<String> listasStr = new ListasEnlazadas<>();
         Random random2 = new Random();
-        
+
         Lista<String> listaStr1 = new Lista<>();
         Lista<String> listaStr2 = new Lista<>();
 
         // Palabras para generar strings aleatorios
-        String[] palabras = {"Sol", "Luna", "Agua", "Fuego", "Tierra", "Viento", "Flor", "Nube", "Rayo", "Mar"};
+        String[] palabras = { "Sol", "Luna", "Agua", "Fuego", "Tierra", "Viento", "Flor", "Nube", "Rayo", "Mar" };
 
         // Generar primer valor aleatorio
         String valorStr = palabras[random2.nextInt(palabras.length)];
@@ -219,7 +240,6 @@ public class ListasEnlazadas<T extends Comparable<T>> {
         // 4. insertAlFinal en lista1 (agregar "Extra1") y lista2 (agregar "Extra2")
         listasStr.insertarAlFinal(listaStr1.getFirst(), "Tormenta");
         listasStr.insertarAlFinal(listaStr2.getFirst(), "Planta");
-        listasStr.insertarAlFinal(listaStr2.getFirst(), "Trueno");
 
         // 5. sonIguales (otra vez)
         listasStr.sonIguales(listaStr1, listaStr2);
