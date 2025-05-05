6d46998a1fdb551d42b748fe49e5f86c1f4a2139
aar00pium <aar00pium@gmail.com>
2025-05-04 22:30:37 -0500

    ej1

---
+++ SESION06/ejercicio1/StackLink.java
@@ -0,0 +1,68 @@
+package SESION06.ejercicio1;
+
+import SESION06.ActividadPila.ExceptionIsEmpty;
+import SESION06.ActividadPila.Stack;
+
+public class StackLink<E> implements Stack<E> {
+    private Node<E> top; // Nodo superior de la pila
+    private int size;     // Tamaño de la pila
+
+    public StackLink() {
+        this.top = null;
+        this.size = 0;
+    }
+
+    @Override
+    public void push(E x) {
+        Node<E> newNode = new Node<>(x);
+        newNode.setNext(top);
+        top = newNode;
+        size++;
+    }
+
+    @Override
+    public E pop() throws ExceptionIsEmpty {
+        if (isEmpty()) {
+            throw new ExceptionIsEmpty("La pila está vacía");
+        }
+        E data = top.getData();
+        top = top.getNext();
+        size--;
+        return data;
+    }
+
+    @Override
+    public E top() throws ExceptionIsEmpty {
+        if (isEmpty()) {
+            throw new ExceptionIsEmpty("La pila está vacía");
+        }
+        return top.getData();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        if (isEmpty()) {
+            return "Pila vacía";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        Node<E> current = top;
+        while (current != null) {
+            sb.append(current.getData());
+            if (current.getNext() != null) {
+                sb.append(" -> ");
+            }
+            current = current.getNext();
+        }
+        return sb.toString();
+    }
+}
